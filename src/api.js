86169e847443a1529c6a12284e7f984e9b252e66
galal mohammed khira <galalkhira122@gmail.com>
2026-09-21 18:42:05 +0300

    Improve attendance save success notification

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 const URL =
-"https://script.google.com/macros/s/AKfycbxOjU5FZRbcjvcI1hmzltss_KpAYrMe6FPUZcffDf3-OL5ZApetsCN6favAs1pzCjm-/exec";
+"https://script.google.com/macros/s/AKfycbz7mjuM0cEl73zlOl8F3X2GeDXPzIKMnnsYMCe-dvmr9o60bSWMz0P3a4oX4z3xVpQY/exec";
 
 export async function callAPI(
   action,
